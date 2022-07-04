27141ce2dd7d3ac95b8a5d269fc68dd3af28ad07
tutorial6 <manapure.vaibhav542@gmail.com>
2022-07-04 14:31:34 +0530

    UC2 Solution Added

--- DataStructure/DataStructure/Program.cs
+++ DataStructure/DataStructure/Program.cs
@@ -8,16 +8,34 @@ namespace DataStructure
         {
             Console.WriteLine("HashTable Program- ");
 
-            MyMapNode<string, string> hash = new MyMapNode<string, string>(5);
-            hash.Add("0", "To");
-            hash.Add("1", "be");
-            hash.Add("2", "or");
-            hash.Add("3", "not");
-            hash.Add("4", "to");
-            hash.Add("5", "be");
+            MyMapNode<string, string> hash = new MyMapNode<string, string>(19);
+            hash.Add("0", "Paranoids");
+            hash.Add("1", "are");
+            hash.Add("2", "not");
+            hash.Add("3", "paranoid");
+            hash.Add("4", "because");
+            hash.Add("5", "they");
+            hash.Add("6", "are");
+            hash.Add("7", "paranoid");
+            hash.Add("8", "but");
+            hash.Add("9", "because");
+            hash.Add("10", "they");
+            hash.Add("11", "keep");
+            hash.Add("12", "putting");
+            hash.Add("13", "themselves");
+            hash.Add("14", "deliberately");
+            hash.Add("15", "into");
+            hash.Add("16", "paranoid");
+            hash.Add("17", "avoidable");
+            hash.Add("18", "situations");
 
-            string hash5 = hash.Get("5");
-            Console.WriteLine("5th index value is: " + hash5);
+
+            string hash1 = hash.Get("12");
+            Console.WriteLine("12th Index Value is :" + hash1);
+
+
+            string hash2 = hash.Get("16");
+            Console.WriteLine("16th Index Value is :" + hash2);
         }
     }
 }
